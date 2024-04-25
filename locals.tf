1ee4a6a76cf28c7e10615bb325e59dd9badf23ad
Byron E. Blank <byronbla@gmail.com>
2024-04-25 15:33:10 -0400

    Add files via upload

---
+++ locals.tf
@@ -0,0 +1,4 @@
+locals {
+  resource_group_name = "1-2a42409b-playground-sandbox"
+  location = "East US"
+}
---
+++ main.tf
@@ -0,0 +1,122 @@
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = local.location
+  resource_group_name = local.resource_group_name
+}
+
+resource "azurerm_subnet" "bastion" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = local.resource_group_name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.224/27"]
+}
+
+resource "azurerm_public_ip" "bastion" {
+  name                = "examplepip"
+  location            = local.location
+  resource_group_name = local.resource_group_name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_bastion_host" "example" {
+  name                = "examplebastion"
+  location            = local.location
+  resource_group_name = local.resource_group_name
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.bastion.id
+    public_ip_address_id = azurerm_public_ip.bastion.id
+  }
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = local.resource_group_name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = local.location
+  resource_group_name = local.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+#Create KeyVault ID
+resource "random_id" "kvname" {
+  byte_length = 5
+  prefix = "keyvault"
+}
+
+
+data "azurerm_client_config" "current" {}
+resource "azurerm_key_vault" "kv1" {
+  name                        = random_id.kvname.hex
+  location                    = local.location
+  resource_group_name         = local.resource_group_name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+  sku_name = "standard"
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+    key_permissions = [
+      "Get",
+    ]
+    secret_permissions = [
+      "Get", "Backup", "Delete", "List", "Purge", "Recover", "Restore", "Set",
+    ]
+    storage_permissions = [
+      "Get",
+    ]
+  }
+}
+
+#Create KeyVault VM password
+resource "random_password" "vmpassword" {
+  length = 20
+  special = true
+}
+#Create Key Vault Secret
+resource "azurerm_key_vault_secret" "vmpassword" {
+  name         = "vmpassword"
+  value        = random_password.vmpassword.result
+  key_vault_id = azurerm_key_vault.kv1.id
+  depends_on = [ azurerm_key_vault.kv1 ]
+
+}
+
+resource "azurerm_windows_virtual_machine" "example" {
+  name                = "example-machine"
+  resource_group_name = local.resource_group_name
+  location            = local.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = azurerm_key_vault_secret.vmpassword.value
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
